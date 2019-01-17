ce0330eb5e8357b1f1fc172946199dde150a0a75
Nat Burgwyn <burgwyn@gmail.com>
2019-01-17 11:51:54 -0500

    Upate login screen

--- public/script.js
+++ public/script.js
@@ -2,10 +2,10 @@ function login(username, password) {
 
   $.ajax({
     type:"POST",
-    url:"https://api.dev.talentmap.us/api/v1/accounts/token/",
+    url:"http://localhost:8000/api/v1/accounts/token/",
     data: "username=" + username + "&password=" + password,
     success: function(data) {
-      window.location.href = '/talentmap/tokenValidation?tmApiToken=' + data.token
+      window.location.href = '/tokenValidation?tmApiToken=' + data.token
     },
     error: function(){
       alert('Request failed');
@@ -14,3 +14,8 @@ function login(username, password) {
   });
 
 };
+
+function submitLogin(e) {
+  e.preventDefault();
+  login(document.getElementById('username'), document.getElementById('password'));
+}
