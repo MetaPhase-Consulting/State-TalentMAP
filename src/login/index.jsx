aa82f41b85b5664c75681b3d841b7661b3e19399
Nat Burgwyn <burgwyn@gmail.com>
2019-01-17 11:52:33 -0500

    Simplify index

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 import { connect } from 'react-redux';
-import { keys, pick } from 'lodash';
 
 import TokenValidation from './Components/TokenValidation';
 
 import { tokenValidationRequest } from './actions';
 import { auth } from './sagas';
 
-// Get functional component prop-type config
-const props$ = {
-  types: TokenValidation.propTypes,
-  defaults: TokenValidation.defaultProps,
-};
-
-props$.keys = keys(props$.types);
-
 const Login = (props) => {
-  const Element = TokenValidation;
   const isSignedIn = !!auth.get();
 
-  // Just to be safe we don't too much automated injecting that we'll just take the ones that
-  // are configured on the components
-  const options = pick(props, props$.keys);
   return (!isSignedIn ?
-    <Element {...options} /> :
+    <TokenValidation {...props} /> :
     <Redirect to="/" />
   );
 };
 
-Login.propTypes = props$.types;
-Login.defaultProps = props$.defaults;
+Login.propTypes = TokenValidation.propTypes;
+Login.defaultProps = TokenValidation.defaultProps;
 
 const mapStateToProps = state => ({
   login: state.login,
