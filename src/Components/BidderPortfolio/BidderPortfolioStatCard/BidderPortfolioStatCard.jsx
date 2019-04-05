c4e18d18a308fcae46dcc5d4b71441b2680f19dd
Mike Joyce <mjoyce91@gmail.com>
2019-04-05 08:35:07 -0400

    Fallback for no grade

--- src/Components/BidderPortfolio/BidderPortfolioStatCard/BidderPortfolioStatCard.jsx
+++ src/Components/BidderPortfolio/BidderPortfolioStatCard/BidderPortfolioStatCard.jsx
@@ -6,7 +6,7 @@ import { BIDDER_OBJECT } from '../../../Constants/PropTypes';
 import BoxShadow from '../../BoxShadow';
 import SkillCodeList from '../../SkillCodeList';
 import { getPostName } from '../../../utilities';
-import { NO_POST } from '../../../Constants/SystemMessages';
+import { NO_POST, NO_GRADE } from '../../../Constants/SystemMessages';
 import ClientBadgeList from '../ClientBadgeList';
 import StaticDevContent from '../../StaticDevContent';
 
@@ -29,7 +29,7 @@ const BidderPortfolioStatCard = ({ userProfile }) => {
           <dt>Skill:</dt><dd><SkillCodeList skillCodes={userProfile.skills} /></dd>
         </div>
         <div className="stat-card-data-point">
-          <dt>Grade:</dt><dd>{userProfile.grade}</dd>
+          <dt>Grade:</dt><dd>{userProfile.grade || NO_GRADE}</dd>
         </div>
         <div className="stat-card-data-point">
           <dt>Post:</dt><dd>{currentAssignmentText}</dd>
