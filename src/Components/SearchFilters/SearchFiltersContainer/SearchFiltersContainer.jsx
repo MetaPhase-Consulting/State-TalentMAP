29844dab15e4fb9ae6cd1157a722b03daafb3c68
Rob Tirserio <rtirserio@gmail.com>
2019-05-28 08:07:21 -0400

    fix: correct search filter names to match cycle position api changes

--- src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
+++ src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.jsx
@@ -53,10 +53,10 @@ class SearchFiltersContainer extends Component {
     } else {
       config = {
         ...config,
-        is_available_in_bidcycle: null,
-        is_available_in_current_bidcycle: null,
-        is_domestic: null,
-        post__in: null,
+        position__is_available_in_bidcycle: null,
+        position__is_available_in_current_bidcycle: null,
+        position__is_domestic: null,
+        position__post__in: null,
         projectedVacancy: value,
       };
     }
--- src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.test.jsx
+++ src/Components/SearchFilters/SearchFiltersContainer/SearchFiltersContainer.test.jsx
@@ -123,10 +123,10 @@ describe('SearchFiltersContainerComponent', () => {
     // check when value !== 'open'
     wrapper.instance().onProjectedVacancyFilterClick('pv');
     expect(toggleValue.value).toEqual({
-      is_available_in_bidcycle: null,
-      is_available_in_current_bidcycle: null,
-      is_domestic: null,
-      post__in: null,
+      position__is_available_in_bidcycle: null,
+      position__is_available_in_current_bidcycle: null,
+      position__is_domestic: null,
+      position__post__in: null,
       projectedVacancy: 'pv',
     });
   });
