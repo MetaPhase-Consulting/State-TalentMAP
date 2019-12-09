7eb3713f62d854db250443de205a42919eb291d5
scott062 <scott.m.burdick@gmail.com>
2019-12-09 18:06:27 -0500

    Update tests and comment out future tests when ready to add feature

--- src/Components/ProfileDashboard/Updates/Updates.test.jsx
+++ src/Components/ProfileDashboard/Updates/Updates.test.jsx
@@ -9,27 +9,33 @@ describe('UpdatesComponent', () => {
     expect(wrapper).toBeDefined();
   });
 
-  it('renders one button on mount', () => {
+  it('renders no button on mount when showEditButton is false', () => {
     const wrapper = shallow(<Updates />);
-    expect(wrapper.find('button').length).toBe(1);
-    expect(wrapper.find('EditButtons').exists()).toBe(false);
+    expect(wrapper.find('button').length).toBe(0);
   });
 
-  it('renders two buttons when the Edit button is clicked', () => {
-    const wrapper = shallow(<Updates />);
-    wrapper.find('button').simulate('click');
-    expect(wrapper.find('button').exists()).toBe(false);
-    expect(wrapper.find('EditButtons').exists()).toBe(true);
-  });
+  // it('renders one button on mount when showEditButton is true ', () => {
+  //   const wrapper = shallow(<Updates />);
+  //   expect(wrapper.find('button').length).toBe(1);
+  //   expect(wrapper.find('EditButtons').exists()).toBe(false);
+  // });
 
-  it('renders one button when enableEdit() is called and then disableEdit() is called', () => {
-    const wrapper = shallow(<Updates />);
-    const instance = wrapper.instance();
-    instance.enableEdit();
-    instance.disableEdit();
-    expect(wrapper.find('button').length).toBe(1);
-    expect(wrapper.find('EditButtons').exists()).toBe(false);
-  });
+  // it('renders two buttons when the Edit button is clicked', () => {
+  //   const wrapper = shallow(<Updates />);
+  //   wrapper.find('button').simulate('click');
+  //   expect(wrapper.find('button').exists()).toBe(false);
+  //   expect(wrapper.find('EditButtons').exists()).toBe(true);
+  // });
+
+  // it('renders one button when enableEdit() is called and then disableEdit() is called', () => {
+  //   const wrapper = shallow(<Updates />);
+  //   const instance = wrapper.instance();
+  //   instance.enableEdit();
+  //   instance.disableEdit();
+  //   expect(wrapper.find('button').length).toBe(1);
+  //   expect(wrapper.find('EditButtons').exists()).toBe(false);
+  // });
+
 
   it('matches snapshot', () => {
     const wrapper = shallow(<Updates />);
