aa346748efb67c7b6f0e7f95bea005fc4ea7c42c
Mike Joyce <joyce_michael@bah.com>
2021-08-26 12:55:50 -0400

    Tweaks to Languages component

--- src/Components/ProfileDashboard/Languages/Languages.jsx
+++ src/Components/ProfileDashboard/Languages/Languages.jsx
@@ -4,26 +4,29 @@ import InformationDataPoint from '../InformationDataPoint';
 
 const Languages = props => {
   const { languagesArray } = props;
+  const languagesArray$ = languagesArray || [];
 
   return (
     <div className="usa-grid-full profile-section-container languages-container">
       <div className="usa-grid-full section-padded-inner-container">
         <div className="usa-width-one-whole">
-          <SectionTitle title="Language History" len={languagesArray.length} icon="language" />
+          <SectionTitle title="Language History" len={languagesArray$.length} icon="language" />
         </div>
+        {
+          !languagesArray$.length &&
+          <div>No language history</div>
+        }
         <div className="languages-list-container">
-          {languagesArray.map(l => (
-            <>
-              <InformationDataPoint
-                title={l.name}
-                content={
-                  <div className="language-details">
-                    {`Reading: ${l.reading} | Speaking: ${l.speaking}`}
-                    <span>{`Test Date: ${l.date}`}</span>
-                  </div>
-                }
-              />
-            </>
+          {languagesArray$.map(l => (
+            <InformationDataPoint
+              title={l.name || 'N/A'}
+              content={
+                <div className="language-details">
+                  {`Reading: ${l.reading || 'N/A'} | Speaking: ${l.speaking || 'N/A'}`}
+                  <span>{`Test Date: ${l.date || 'N/A'}`}</span>
+                </div>
+              }
+            />
           ))}
         </div>
       </div>
@@ -32,7 +35,16 @@ const Languages = props => {
 };
 
 Languages.propTypes = {
-  languagesArray: PropTypes.shape([]),
+  languagesArray: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        name: PropTypes.string,
+        reading: PropTypes.string,
+        speaking: PropTypes.string,
+        date: PropTypes.string,
+      },
+    ),
+  ),
 };
 
 Languages.defaultProps = {
