9b64a6a39fb8e9da0397e4d271897e53b294f7c7
Cory Covington <ccovington@metaphaseconsulting.com>
2021-12-02 20:28:12 -0500

    more styling updates & fake data

--- src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
+++ src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.jsx
@@ -9,11 +9,106 @@ const EmployeeAgendaSearch = ({ isCDO }) => {
   const [cardView, setCardView] = useState(false);
   const view = cardView ? 'card' : 'grid';
 
+  const data = [
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '04/2022',
+      handshake: true, // for testing only
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '04/2025',
+      handshake: false,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '09/2023',
+      handshake: true,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '11/2022',
+      handshake: true,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '07/2024',
+      handshake: true,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '09/2023',
+      handshake: true,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '03/2023',
+      handshake: true,
+    },
+    {
+      agendaStatus: 'Complete',
+      author: 'Last, First MI Author',
+      bidder: 'Last, First MI',
+      cdo: 'Last, First MI CDO',
+      currentPost: 'Paris (EUR)',
+      futurePost: 'Djbouti (AF)',
+      panelDate: '12/25/2021',
+      ted: '06/2023',
+      handshake: true,
+    },
+  ];
+
   return (
     <div className="usa-grid-full profile-content-inner-container">
       <ProfileSectionTitle title="Employee Agenda Search" icon="user-circle-o" />
       <div className="usa-grid-full employee-agenda-card-list">
-        <EmployeeAgendaSearchCards isCDO={isCDO} />
+        {
+          data.map(result => (
+            <EmployeeAgendaSearchCards results={result} isCDO={isCDO} />
+          ))
+        }
       </div>
       {/* {
         cardView &&
--- src/Components/Agenda/EmployeeAgendaSearchCards/EmployeeAgendaSearchCards.jsx
+++ src/Components/Agenda/EmployeeAgendaSearchCards/EmployeeAgendaSearchCards.jsx
@@ -3,23 +3,14 @@ import { Link } from 'react-router-dom';
 import { checkFlag } from 'flags';
 import FA from 'react-fontawesome';
 import { Tooltip } from 'react-tippy';
+import { BIDDER_OBJECT } from 'Constants/PropTypes'; // for dummy data purposes only
 import BoxShadow from '../../BoxShadow';
 import { Handshake } from '../../Ribbon';
+import InteractiveElement from '../../InteractiveElement';
 
-const EmployeeAgendaSearchCards = ({ isCDO }) => {
+const EmployeeAgendaSearchCards = ({ isCDO, results }) => {
   const userRole = isCDO ? 'cdo' : 'ao';
   const useCDOBidding = () => checkFlag('flags.cdo_bidding');
-
-  const data = {
-    agendaStatus: 'Complete',
-    author: 'Last, First MI Author',
-    bidder: 'Last, First MI',
-    cdo: 'Last, First MI CDO',
-    currentPost: 'Paris (EUR)',
-    futurePost: 'Djbouti (AF)',
-    panelDate: '12/25/2021',
-    ted: '11/29/2021',
-  };
 
   return (
     <div className="employee-agenda-card-container">
@@ -40,47 +31,49 @@ const EmployeeAgendaSearchCards = ({ isCDO }) => {
           </div>
           <div>
             <h3>
-              <Link to="/profile/public/4">{data.bidder}</Link>
+              {/* may want to disable link for prod, only show name? */}
+              <Link to="/profile/public/4">{results.bidder}</Link>
             </h3>
           </div>
           <div className="employee-agenda-card-data-point-top">
             <div className="employee-card-data-point">
               <FA name="building-o" />
-              <dt>ORG:</dt><dd>{data.currentPost} <FA className="org-fa-arrow" name="long-arrow-right" /> {data.futurePost}</dd>
+              <dt>ORG:</dt><dd>{results.currentPost} <FA className="org-fa-arrow" name="long-arrow-right" /> {results.futurePost}</dd>
             </div>
             <div className="employee-card-data-point">
               <FA name="clock-o" />
-              <dt>TED:</dt><dd>{data.ted}</dd>
+              <dt>TED:</dt><dd>{results.ted}</dd>
             </div>
             <div className="employee-card-data-point">
               <FA name="user-o" />
-              <dt>CDO:</dt><dd>{data.cdo}</dd>
+              <dt>CDO:</dt><dd>{results.cdo}</dd>
             </div>
           </div>
           <div className="employee-card-data-point">
             <FA name="pencil-square" />
-            <dt>Author:</dt><dd className="author-name">{data.author}</dd>
+            <dt>Author:</dt><dd className="author-name">{results.author}</dd>
           </div>
           <div className="employee-card-data-point">
             <FA name="calendar-o" />
-            <dt>Panel Meeting Date:</dt><dd>{data.panelDate}</dd>
+            <dt>Panel Meeting Date:</dt><dd>{results.panelDate}</dd>
           </div>
           <div className="employee-card-data-point">
             <FA name="sticky-note-o" />
-            <dt>Agenda Status:</dt><dd>{data.agendaStatus}</dd>
+            <dt>Agenda Status:</dt><dd>{results.agendaStatus}</dd>
           </div>
         </div>
         <div className="employee-agenda-card-top-bottom">
           {useCDOBidding() &&
         <div className="button-container" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-          <div className="eas-button">
-            <span>
-              <Link to={`/profile/${userRole}/agendaitemhistory/perdet`}>View History</Link>
-            </span>
+          <div className="view-agenda-item-container">
+            <InteractiveElement className="view-agenda-item-button">
+              <Link className="view-agenda-item-text" to={`/profile/${userRole}/agendaitemhistory/perdet`}>View History</Link>
+            </InteractiveElement>
           </div>
-          <div className="employee-agenda-search-button">
-            {/* styling may change since this will be a component */}
+          <div className="create-pai-box-container">
+            <InteractiveElement className="create-pai-box-button">
                 Create Agenda Item
+            </InteractiveElement>
           </div>
         </div>}
         </div>
@@ -91,10 +84,12 @@ const EmployeeAgendaSearchCards = ({ isCDO }) => {
 
 EmployeeAgendaSearchCards.propTypes = {
   isCDO: PropTypes.bool,
+  results: BIDDER_OBJECT.isRequired,
 };
 
 EmployeeAgendaSearchCards.defaultProps = {
   isCDO: false,
+  results: {},
 };
 
 export default EmployeeAgendaSearchCards;
