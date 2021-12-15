4b5a2f2554062c15c6e81e18bebd24002c2f67ea
scott062 <scott.m.burdick@gmail.com>
2021-12-15 11:26:46 -0500

    Fix code climate errors

--- src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.test.jsx
+++ src/Components/Agenda/EmployeeAgendaSearch/EmployeeAgendaSearch.test.jsx
@@ -11,23 +11,35 @@ const mockStore = configureStore(middlewares);
 
 describe('EmployeeAgendaSearchComponent', () => {
   it('is defined', () => {
-    const wrapper = TestUtils.renderIntoDocument(<Provider store={mockStore({})}><MemoryRouter>
-      <EmployeeAgendaSearch isCDO />
-    </MemoryRouter></Provider>);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <EmployeeAgendaSearch isCDO />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot when CDO', () => {
-    const wrapper = TestUtils.renderIntoDocument(<Provider store={mockStore({})}><MemoryRouter>
-      <EmployeeAgendaSearch isCDO />
-    </MemoryRouter></Provider>);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <EmployeeAgendaSearch isCDO />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
   it('matches snapshot when not CDO', () => {
-    const wrapper = TestUtils.renderIntoDocument(<Provider store={mockStore({})}><MemoryRouter>
-      <EmployeeAgendaSearch isCDO={false} />
-    </MemoryRouter></Provider>);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <EmployeeAgendaSearch isCDO={false} />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
