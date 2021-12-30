5a88bdda96cf70a6cb81a94f9237677a0dcd65e1
Mike Joyce <joyce_michael@bah.com>
2021-12-30 18:13:48 -0500

    get and display client name on aih

--- src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
+++ src/Components/Agenda/AgendaItemHistory/AgendaItemHistory.jsx
@@ -6,6 +6,7 @@ import { get } from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
 import { aihFetchData } from 'actions/agendaItemHistory';
+import { fetchClient } from 'actions/client';
 import { useMount, usePrevious } from 'hooks';
 import Spinner from 'Components/Spinner';
 import Alert from 'Components/Alert';
@@ -18,9 +19,12 @@ import ScrollUpButton from '../../ScrollUpButton';
 
 const AgendaItemHistory = (props) => {
   const sorts = AGENDA_ITEM_HISTORY_FILTERS;
+  const id = get(props, 'match.params.id'); // client's perdet
 
   const [cardView, setCardView] = useState(false);
   const [sort, setSort] = useState(sorts.defaultSort);
+  const [client, setClient] = useState('');
+  const [clientIsLoading, setClientIsLoading] = useState(false);
   const view = cardView ? 'card' : 'grid';
 
   const aih = useSelector(state => state.aih);
@@ -31,14 +35,24 @@ const AgendaItemHistory = (props) => {
   const dispatch = useDispatch();
 
   const getData = () => {
-    const id = get(props, 'match.params.id'); // client's perdet
     dispatch(aihFetchData(id, sort));
   };
 
   const prevSort = usePrevious(sort);
 
+  const getClient = () => {
+    setClientIsLoading(true);
+    Promise.all([fetchClient(id)])
+      .then((res) => {
+        setClient(get(res, '[0].name'));
+        setClientIsLoading(false);
+      })
+      .catch(() => setClientIsLoading(false));
+  };
+
   useMount(() => {
     getData();
+    getClient();
   });
 
   useEffect(() => {
@@ -47,10 +61,19 @@ const AgendaItemHistory = (props) => {
     }
   }, [sort]);
 
+  const isLoading$ = isLoading || clientIsLoading;
+  let title = 'Agenda Item History';
+  if (client) {
+    title = `${client}'s ${title}`;
+  }
+  if (clientIsLoading) {
+    title = '';
+  }
+
   return (
     <div className="agenda-item-history-container">
       <div className="usa-grid-full profile-content-inner-container">
-        <ProfileSectionTitle title="Rehman, Tarek - Agenda Item History" icon="user-circle-o" />
+        <ProfileSectionTitle title={title} icon="user-circle-o" />
         <BackButton />
         <div className="usa-grid-full portfolio-controls">
           <div className="usa-width-one-whole results-dropdown agenda-controls-container">
@@ -68,14 +91,18 @@ const AgendaItemHistory = (props) => {
           </div>
         </div>
         {
-          isLoading && !hasErrored && <div className="ai-history-cards-container"><Spinner type="homepage-position-results" size="big" /></div>
+          isLoading$ && !hasErrored && <div className="ai-history-cards-container"><Spinner type="homepage-position-results" size="big" /></div>
         }
         {
-          !isLoading && hasErrored &&
+          !isLoading$ && !hasErrored && (aih || []).length < 1 &&
+          <div className="ai-history-cards-container"><Alert type="info" title="No agenda items" messages={[{ body: 'This user does not have any agenda items.' }]} /></div>
+        }
+        {
+          !isLoading$ && hasErrored &&
           <div className="ai-history-cards-container"><Alert type="error" title="Error loading agenda history" messages={[{ body: 'Please try again.' }]} /></div>
         }
         {
-          !isLoading && !hasErrored &&
+          !isLoading$ && !hasErrored && (aih || []).length > 0 &&
             <>
               {
                 cardView &&
