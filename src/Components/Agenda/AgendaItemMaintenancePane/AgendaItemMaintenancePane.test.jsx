e8093738d8d14c857ec3678d4c2f082c24d9a2ee
elizabeth <elizabeth.jimenez.cs@gmail.com>
2022-06-16 15:10:52 -0400

    test update

--- src/Components/Agenda/AgendaItemMaintenancePane/AgendaItemMaintenancePane.test.jsx
+++ src/Components/Agenda/AgendaItemMaintenancePane/AgendaItemMaintenancePane.test.jsx
@@ -1,15 +1,33 @@
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { MemoryRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import TestUtils from 'react-dom/test-utils';
 import AgendaItemMaintenancePane from './AgendaItemMaintenancePane';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('AgendaItemMaintenancePane Component', () => {
   it('is defined', () => {
-    const wrapper = shallow(<AgendaItemMaintenancePane />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <AgendaItemMaintenancePane />
+        </MemoryRouter>
+      </Provider>);
     expect(wrapper).toBeDefined();
   });
 
   it('matches snapshot', () => {
-    const wrapper = shallow(<AgendaItemMaintenancePane />);
+    const wrapper = TestUtils.renderIntoDocument(
+      <Provider store={mockStore({})}>
+        <MemoryRouter>
+          <AgendaItemMaintenancePane />
+        </MemoryRouter>
+      </Provider>);
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
