effad9b94b7a4e00bbe7632b6d6b0cc79824829f
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2024-06-05 13:10:17 -0400

    TM-6164, update naming for post access

--- src/Components/BureauPage/SearchPostAccess/SearchPostAccess.jsx
+++ src/Components/BureauPage/SearchPostAccess/SearchPostAccess.jsx
@@ -150,7 +150,7 @@ const SearchPostAccess = () => {
     dispatch(searchPostAccessRemove(checkedPostIds));
   };
 
-  const tableHeaderNames = ['Access Type', 'Bureau', 'Post/Org', 'Employee', 'Role', 'Position', 'Title'];
+  const tableHeaderNames = ['Access Type', 'Bureau', 'Organization', 'Employee', 'Role', 'Position', 'Title'];
 
   const handleSelectAll = () => {
     if (!selectAll) {
