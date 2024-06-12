863e3d6e95e1d0c3dad9241f72662862e9649691
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2024-06-12 13:38:04 -0400

    code review

--- src/Components/AoPage/AoPage.jsx
+++ src/Components/AoPage/AoPage.jsx
@@ -26,7 +26,7 @@ const AoPage = () => (
       <Route path="/profile/ao/availablebidders" render={() => <AvailableBidderContainer isCDO={false} isAO />} />
       <Route path="/profile/ao/panelmeetingagendas/:pmID" render={() => <PanelMeetingAgendas isAO />} />
       <Route path="/profile/ao/cyclemanagement" render={() => <CycleManagement isAO />} />
-      <Route path="/profile/ao/projectedvacancy" render={() => <ProjectedVacancy isCDO={false} viewType="ao" />} />
+      <Route path="/profile/ao/projectedvacancy" render={() => <ProjectedVacancy viewType="ao" />} />
       <Route path="/profile/ao/assignmentcycle/:id" render={() => <AssignmentCycleEdit isAO />} />
       <Route path="/profile/ao/cyclepositionsearch/:id" render={() => <CyclePositionSearch isAO />} />
       <Route path="/profile/ao/cycleclassifications/" render={() => <CycleClassifications isAO />} />
