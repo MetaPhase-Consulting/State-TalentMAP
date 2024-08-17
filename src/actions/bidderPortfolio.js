23f52ca48b76e51b4a042339c718f73887949401
musiqe31 <agbakwucharles@gmail.com>
2024-08-16 19:23:32 -0500

    create new url

--- src/actions/bidderPortfolio.js
+++ src/actions/bidderPortfolio.js
@@ -374,7 +374,7 @@ export function saveBidderPortfolioSelections(client) {
     dispatch(bidderPortfolioSeasonsIsLoading(true));
     dispatch(bidderPortfolioSeasonsHasErrored(false));
     api()
-      .post('/fsbid/client/', client)
+      .post('/fsbid/client/update/', client)
       .then(({ data }) => {
         batch(() => {
           dispatch(bidderPortfolioSeasonsHasErrored(false));
