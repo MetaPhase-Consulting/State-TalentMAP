38e9c7cbe34139ed729052dee84e5862c23f56cb
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-09-10 16:25:05 -0400

    Fixed to properly display routing items and change/add items

--- src/Components/AssignmentsSeparations/NotificationCard/Tabs/Routing.jsx
+++ src/Components/AssignmentsSeparations/NotificationCard/Tabs/Routing.jsx
@@ -5,20 +5,20 @@ import { Row } from 'Components/Layout';
 
 const Routing = (props) => {
   const {
-    getCableValue,
-    modCableValue,
+    routingValue,
+    modRoutingValue,
     postOptions,
     precedenceOptions,
     organizationOptions,
   } = props;
 
-  const postDropdown = (key) => (
+  const postDropdown = (nmdSeqNum, value) => (
     <div className="position-form--label-input-container width-300">
-      <label htmlFor={`post-${key}`}>Cable Post</label>
+      <label htmlFor={`post-${nmdSeqNum}`}>Cable Post</label>
       <select
-        id={`post-${key}`}
-        value=""
-        onChange={() => { }}
+        id={`post-${nmdSeqNum}`}
+        value={value || ''}
+        onChange={(e) => modRoutingValue(nmdSeqNum, 'CP_SEQ_NUM', e.target.value)}
       >
         <option value="" disabled>Select Post</option>
         {postOptions.map(b => (
@@ -28,13 +28,13 @@ const Routing = (props) => {
     </div>
   );
 
-  const precedenceDropdown = (key) => (
+  const precedenceDropdown = (nmdSeqNum, value) => (
     <div className="position-form--label-input-container width-300">
-      <label htmlFor={`precedence-${key}`}>Precedence</label>
+      <label htmlFor={`precedence-${nmdSeqNum}`}>Precedence</label>
       <select
-        id={`precedence-${key}`}
-        value=""
-        onChange={() => { }}
+        id={`precedence-${nmdSeqNum}`}
+        value={value || ''}
+        onChange={(e) => modRoutingValue(nmdSeqNum, 'PT_CODE', e.target.value)}
       >
         <option value="" disabled>Select Precedence</option>
         {precedenceOptions.map(b => (
@@ -48,74 +48,108 @@ const Routing = (props) => {
     <div className="position-content position-form input-container">
       <div className="notification-card__sub-section">
         <span className="sub-header">Action</span>
-        <div className="display-flex justify-space-between width-700 mt-20">
-          {postDropdown()}
-          {precedenceDropdown()}
-        </div>
-        <Row fluid className="position-content--textArea width-700  mt-20">
-          <span className="definition-title">Slug Text</span>
-          <Linkify properties={{ target: '_blank' }}>
-            <TextareaAutosize
-              maxRows={4}
-              minRows={1}
-              maxLength="500"
-              name="subject"
-              placeholder="No Description"
-              defaultValue=""
-              onChange={() => { }}
-              draggable={false}
-            />
-          </Linkify>
-          <div className="word-count">
-            0 / 500
+        {routingValue.filter(v => v.DT_CODE === 'A').map(action => (
+          <div className="routing-sub-section">
+            <div className="display-flex justify-space-between width-700">
+              {postDropdown(action?.NMD_SEQ_NUM, action?.CP_SEQ_NUM)}
+              {precedenceDropdown(action?.NMD_SEQ_NUM, action?.PT_CODE)}
+            </div>
+            <Row fluid className="position-content--textArea width-700 mt-10">
+              <span className="definition-title">Slug Text</span>
+              <Linkify properties={{ target: '_blank' }}>
+                <TextareaAutosize
+                  maxRows={4}
+                  minRows={1}
+                  maxLength="500"
+                  name="subject"
+                  placeholder="No Description"
+                  value={action?.NMD_SLUG_TEXT}
+                  onChange={(e) => modRoutingValue(action?.NMD_SEQ_NUM, 'NMD_SLUG_TEXT', e.target.value)}
+                  draggable={false}
+                />
+              </Linkify>
+              <div className="word-count">
+                {action?.NMD_SLUG_TEXT?.length} / 500
+              </div>
+            </Row>
           </div>
-        </Row>
-        <a href="tbd" rel="PMD" target="_blank">Add Another Action</a>
+        ))}
+        <div className="standard-add-button pt-12">
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={() => modRoutingValue(null, 'DT_CODE', 'A')}
+          >
+            Add Another Action
+          </a>
+        </div>
         <div className="content-divider mt-40" />
       </div>
       <div className="notification-card__sub-section">
         <span className="sub-header">Information</span>
-        <div className="display-flex justify-space-between width-700 mt-20">
-          {postDropdown()}
-          {precedenceDropdown()}
-        </div>
-        <Row fluid className="position-content--textArea width-700  mt-20">
-          <span className="definition-title">Slug Text</span>
-          <Linkify properties={{ target: '_blank' }}>
-            <TextareaAutosize
-              maxRows={4}
-              minRows={1}
-              maxLength="500"
-              name="subject"
-              placeholder="No Description"
-              defaultValue=""
-              onChange={() => { }}
-              draggable={false}
-            />
-          </Linkify>
-          <div className="word-count">
-            0 / 500
+        {routingValue.filter(v => v.DT_CODE === 'I').map(info => (
+          <div className="routing-sub-section">
+            <div className="display-flex justify-space-between width-700">
+              {postDropdown(info?.NMD_SEQ_NUM, info?.CP_SEQ_NUM)}
+              {precedenceDropdown(info?.NMD_SEQ_NUM, info?.PT_CODE)}
+            </div>
+            <Row fluid className="position-content--textArea width-700 mt-10">
+              <span className="definition-title">Slug Text</span>
+              <Linkify properties={{ target: '_blank' }}>
+                <TextareaAutosize
+                  maxRows={4}
+                  minRows={1}
+                  maxLength="500"
+                  name="subject"
+                  placeholder="No Description"
+                  value={info?.NMD_SLUG_TEXT}
+                  onChange={(e) => modRoutingValue(info?.NMD_SEQ_NUM, 'NMD_SLUG_TEXT', e.target.value)}
+                  draggable={false}
+                />
+              </Linkify>
+              <div className="word-count">
+                {info?.NMD_SLUG_TEXT?.length} / 500
+              </div>
+            </Row>
           </div>
-        </Row>
-        <a href="tbd" rel="PMD" target="_blank">Add More Information</a>
+        ))}
+        <div className="standard-add-button pt-12">
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={() => modRoutingValue(null, 'DT_CODE', 'I')}
+          >
+            Add More Information
+          </a>
+        </div>
         <div className="content-divider mt-40" />
       </div>
       <div className="notification-card__sub-section">
         <span className="sub-header">Distribution</span>
-        <div className="position-form--label-input-container width-200">
-          <label htmlFor="organization">Organization</label>
-          <select
-            id="organization"
-            value={getCableValue('DISTRIBUTION')}
-            onChange={() => modCableValue('DISTRIBUTION', '')}
+        {routingValue.filter(v => v.DT_CODE === 'D').map(dist => (
+          <div className="position-form--label-input-container width-200">
+            <label htmlFor="organization">Organization</label>
+            <select
+              id="organization"
+              value={dist?.ORG_CODE}
+              onChange={(e) => modRoutingValue(dist?.NMD_SEQ_NUM, 'ORG_CODE', e.target.value)}
+            >
+              <option value="" disabled>Select Organization</option>
+              {organizationOptions.map(b => (
+                <option key={b.ORG_CODE} value={b.ORG_CODE}>{b.ORGS_SHORT_DESC}</option>
+              ))}
+            </select>
+          </div>
+        ))}
+        <div className="standard-add-button pt-12">
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={() => modRoutingValue(null, 'DT_CODE', 'D')}
           >
-            <option value="" disabled>Select Organization</option>
-            {organizationOptions.map(b => (
-              <option key={b.ORG_CODE} value={b.ORG_CODE}>{b.ORGS_SHORT_DESC}</option>
-            ))}
-          </select>
+            Add Another Distribution
+          </a>
         </div>
-        <a href="tbd" rel="PMD" target="_blank">Add Another Distribution</a>
         <div className="content-divider" />
       </div>
     </div>
@@ -123,16 +157,16 @@ const Routing = (props) => {
 };
 
 Routing.propTypes = {
-  getCableValue: PropTypes.func,
-  modCableValue: PropTypes.func,
+  routingValue: PropTypes.arrayOf(PropTypes.shape({})),
+  modRoutingValue: PropTypes.func,
   postOptions: PropTypes.arrayOf(PropTypes.shape({})),
   precedenceOptions: PropTypes.arrayOf(PropTypes.shape({})),
   organizationOptions: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 Routing.defaultProps = {
-  getCableValue: undefined,
-  modCableValue: undefined,
+  routingValue: undefined,
+  modRoutingValue: undefined,
   postOptions: undefined,
   precedenceOptions: undefined,
   organizationOptions: undefined,
