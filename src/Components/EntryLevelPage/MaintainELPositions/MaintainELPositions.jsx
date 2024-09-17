11ccaf8d05f0702c92108450877cd1a5081adc79
Felicia Imbriano <fimbriano@metaphaseconsulting.com>
2024-09-17 17:02:51 -0400

    add el filter api call

--- src/Components/EntryLevelPage/MaintainELPositions/MaintainELPositions.jsx
+++ src/Components/EntryLevelPage/MaintainELPositions/MaintainELPositions.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import FA from 'react-fontawesome';
 import Picky from 'react-picky';
+import { entryLevelFiltersFetchData } from 'actions/entryLevel';
 import { renderSelectionList } from 'utilities';
 import PositionManagerSearch from 'Components/BureauPage/PositionManager/PositionManagerSearch';
 import ProfileSectionTitle from 'Components/ProfileSectionTitle';
 import CheckBox from 'Components/CheckBox';
-import { filter, flatten, isEmpty } from 'lodash';
 import ExportButton from 'Components/ExportButton';
+import MaintainELPositionsTable from 'Components/EntryLevelPage/MaintainELPositionsTable';
 import SelectForm from 'Components/SelectForm';
-import { POSITION_PAGE_SIZES } from 'Constants/Sort';
 import PaginationWrapper from 'Components/PaginationWrapper';
-import MaintainELPositionsTable from '../MaintainELPositionsTable';
+import { filter, flatten, isEmpty } from 'lodash';
+import { POSITION_PAGE_SIZES } from 'Constants/Sort';
+import Spinner from 'Components/Spinner';
 
 const MaintainEntryLevelPositions = () => {
+  const dispatch = useDispatch();
+
   const userSelections = useSelector(state => state.entryLevelSelections);
   const [page, setPage] = useState(userSelections.page || 1);
   const [limit, setLimit] = useState(userSelections.limit || 10);
@@ -29,16 +33,22 @@ const MaintainEntryLevelPositions = () => {
   const [textSearch, setTextSearch] = useState('');
   const [clearFilters, setClearFilters] = useState(false);
 
-  // const elFiltersList = useSelector(state => state.entryLevelFilters);
-  const tpFilters = []; // = elFiltersList?.tpFilters;
-  const bureauFilters = []; // = elFiltersList?.bureauFilters;
-  const orgFilters = []; // = elFiltersList?.orgFilters;
-  const gradeFilters = []; // = elFiltersList?.gradeFilters;
-  const skillsFilters = []; // = elFiltersList?.skillsFilters;
-  const jcFilters = []; // = elFiltersList?.jcFilters;
-  const languageFilters = []; // = elFiltersList?.languageFilters;
+  // const elFiltersHasErrored = useSelector(state => state.entryLevelFiltersFetchDataErrored);
+  const elFiltersIsLoading = useSelector(state => state.entryLevelFiltersFetchDataLoading);
+  const elFiltersList = useSelector(state => state.entryLevelFilters);
+  const tpFilters = elFiltersList?.tpFilters;
+  const bureauFilters = elFiltersList?.bureauFilters;
+  const orgFilters = elFiltersList?.orgFilters;
+  const gradeFilters = elFiltersList?.gradeFilters;
+  const skillsFilters = elFiltersList?.skillsFilters;
+  const jcFilters = elFiltersList?.jcFilters;
+  const languageFilters = elFiltersList?.languageFilters;
 
   const childRef = useRef();
+
+  useEffect(() => {
+    dispatch(entryLevelFiltersFetchData());
+  }, []);
 
   const fetchAndSet = () => {
     const filters = [
@@ -82,164 +92,188 @@ const MaintainEntryLevelPositions = () => {
     fetchAndSet();
   }, [selectedTps, selectedBureaus, selectedOrgs, selectedGrades, selectedSkills, selectedLanguages, overseas, domestic, textSearch]);
 
+  const isLoading = elFiltersIsLoading;
+  const getOverlay = () => {
+    let toReturn;
+    // if (elPositionsIsLoading) {
+    //   toReturn = <Spinner type="bureau-results" class="homepage-position-results" size="big" />;
+    // } else if (panelMeetingsHasErrored || panelMeetingsFiltersHasErrored) {
+    //   toReturn = <Alert type="error" title="Error loading panel meetings" messages={[{ body: 'Please try again.' }]} />;
+    // } else if (noPanelMeetingResults) {
+    //   toReturn = <Alert type="info" title="No results found" messages={[{ body: 'Please broaden your search criteria and try again.' }]} />;
+    // }
+    if (toReturn) {
+      return <div className="usa-width-one-whole empl-search-lower-section results-dropdown">{toReturn}</div>;
+    }
+    return false;
+  };
+
   return (
-    <div className="entry-level-page position-search">
-      <div className="usa-grid-full position-search--header">
-        <ProfileSectionTitle title="Entry Level Search" icon="keyboard-o" />
-        <div className="search-bar-container">
-          <PositionManagerSearch
-            ref={childRef}
-            placeHolder="Search using Position Number or Position Title"
-            textSearch={textSearch}
-            onChange={setTextSearch}
-            noButton
-          />
-        </div>
-        <div className="filterby-container">
-          <div className="filterby-label">Filter by:</div>
-          <div className="filterby-clear">
-            {clearFilters &&
+    isLoading ? <Spinner type="bureau-filters" size="small" /> :
+      <>
+        <div className="entry-level-page position-search">
+          <div className="usa-grid-full position-search--header">
+            <ProfileSectionTitle title="Entry Level Search" icon="keyboard-o" />
+            <div className="search-bar-container">
+              <PositionManagerSearch
+                ref={childRef}
+                placeHolder="Search using Position Number or Position Title"
+                textSearch={textSearch}
+                onChange={setTextSearch}
+                noButton
+              />
+            </div>
+            <div className="filterby-container">
+              <div className="filterby-label">Filter by:</div>
+              <div className="filterby-clear">
+                {clearFilters &&
               <button className="unstyled-button" onClick={resetFilters}>
                 <FA name="times" />
                     Clear Filters
               </button>
+                }
+              </div>
+            </div>
+            <div className="usa-width-one-whole position-search--filters--el results-dropdown">
+              <div className="filter-div">
+                <div className="label">Tracking Program:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select TP(s)"
+                  value={selectedTps}
+                  options={tpFilters}
+                  onChange={setSelectedTps}
+                  valueKey="code"
+                  labelKey="code"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Bureau:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Bureau(s)"
+                  value={selectedBureaus}
+                  options={bureauFilters}
+                  onChange={setSelectedBureaus}
+                  valueKey="code"
+                  labelKey="description"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Location/Org:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Organization(s)"
+                  value={selectedOrgs}
+                  options={orgFilters}
+                  onChange={setSelectedOrgs}
+                  valueKey="code"
+                  labelKey="description"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Grade:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Grade(s)"
+                  value={selectedGrades}
+                  options={gradeFilters}
+                  onChange={setSelectedGrades}
+                  valueKey="code"
+                  labelKey="description"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Skill:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Skill(s)"
+                  value={selectedSkills}
+                  options={skillsFilters}
+                  onChange={setSelectedSkills}
+                  valueKey="code"
+                  labelKey="custom_description"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Job Categories:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Job Categories(s)"
+                  value={selectedJobs}
+                  options={jcFilters}
+                  onChange={setSelectedJobs}
+                  valueKey="code"
+                  labelKey="description"
+                />
+              </div>
+              <div className="filter-div">
+                <div className="label">Languages:</div>
+                <Picky
+                  {...pickyProps}
+                  placeholder="Select Language(s)"
+                  value={selectedLanguages}
+                  options={languageFilters}
+                  onChange={setSelectedLanguages}
+                  valueKey="code"
+                  labelKey="description"
+                />
+              </div>
+              <div className="filter-div">
+                <CheckBox
+                  id="overseas"
+                  label="Overseas Only"
+                  value={overseas}
+                  onCheckBoxClick={e => setOverseas(e)}
+                  disabled={domestic}
+                />
+                <CheckBox
+                  id="domestic"
+                  label="Domestic Only"
+                  value={domestic}
+                  onCheckBoxClick={e => setDomestic(e)}
+                  disabled={overseas}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="controls-container results-dropdown">
+            <div className="el-page-header">Maintain Entry Level Positions</div>
+            <div className="position-search-controls--right">
+              <div className="position-search-controls--results">
+                <SelectForm
+                  id="position-manager-num-results"
+                  options={POSITION_PAGE_SIZES.options}
+                  label="Results:"
+                  defaultSort={limit}
+                  onSelectOption={value => setLimit(value.target.value)}
+                />
+              </div>
+              <div className="export-button-container">
+                <ExportButton />
+              </div>
+            </div>
+          </div>
+          <div className="usa-width-one-whole position-search--results">
+            {
+              getOverlay() ||
+            <>
+              <div className="usa-grid-full el-table-container">
+                <MaintainELPositionsTable />
+              </div>
+              <div className="usa-grid-full react-paginate">
+                <PaginationWrapper
+                  pageSize={limit}
+                  onPageChange={p => setPage(p.page)}
+                  forcePage={page}
+                  totalResults={1}
+                />
+              </div>
+            </>
             }
           </div>
         </div>
-        <div className="usa-width-one-whole position-search--filters--el results-dropdown">
-          <div className="filter-div">
-            <div className="label">Tracking Program:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select TP(s)"
-              value={selectedTps}
-              options={tpFilters}
-              onChange={setSelectedTps}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Bureau:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Bureau(s)"
-              value={selectedBureaus}
-              options={bureauFilters}
-              onChange={setSelectedBureaus}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Location/Org:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Organization(s)"
-              value={selectedOrgs}
-              options={orgFilters}
-              onChange={setSelectedOrgs}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Grade:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Grade(s)"
-              value={selectedGrades}
-              options={gradeFilters}
-              onChange={setSelectedGrades}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Skill:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Skill(s)"
-              value={selectedSkills}
-              options={skillsFilters}
-              onChange={setSelectedSkills}
-              valueKey="code"
-              labelKey="custom_description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Job Categories:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Job Categories(s)"
-              value={selectedJobs}
-              options={jcFilters}
-              onChange={setSelectedJobs}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <div className="label">Languages:</div>
-            <Picky
-              {...pickyProps}
-              placeholder="Select Language(s)"
-              value={selectedLanguages}
-              options={languageFilters}
-              onChange={setSelectedLanguages}
-              valueKey="code"
-              labelKey="description"
-            />
-          </div>
-          <div className="filter-div">
-            <CheckBox
-              id="overseas"
-              label="Overseas Only"
-              value={overseas}
-              onCheckBoxClick={e => setOverseas(e)}
-              disabled={domestic}
-            />
-            <CheckBox
-              id="domestic"
-              label="Domestic Only"
-              value={domestic}
-              onCheckBoxClick={e => setDomestic(e)}
-              disabled={overseas}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="controls-container results-dropdown">
-        <div className="el-page-header">Maintain Entry Level Positions</div>
-        <div className="position-search-controls--right">
-          <div className="position-search-controls--results">
-            <SelectForm
-              id="position-manager-num-results"
-              options={POSITION_PAGE_SIZES.options}
-              label="Results:"
-              defaultSort={limit}
-              onSelectOption={value => setLimit(value.target.value)}
-            />
-          </div>
-          <div className="export-button-container">
-            <ExportButton />
-          </div>
-        </div>
-      </div>
-      <div className="usa-width-one-whole position-search--results">
-        <div className="usa-grid-full el-table-container">
-          <MaintainELPositionsTable />
-        </div>
-        <div className="usa-grid-full react-paginate">
-          <PaginationWrapper
-            pageSize={limit}
-            onPageChange={p => setPage(p.page)}
-            forcePage={page}
-            totalResults={1}
-          />
-        </div>
-      </div>
-    </div>
+      </>
   );
 };
 
