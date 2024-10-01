30ac3010d63fb8dcb710223bda578d7c7d2b0cf8
musiqe31 <agbakwucharles@gmail.com>
2024-10-01 18:19:26 -0500

    updated keys

--- src/Components/EntryLevelPage/MaintainELPositionsTable/MaintainELPositionsTable.jsx
+++ src/Components/EntryLevelPage/MaintainELPositionsTable/MaintainELPositionsTable.jsx
@@ -6,7 +6,6 @@ import { entryLevelEdit } from 'actions/entryLevel';
 import { format } from 'date-fns-v2';
 
 const MaintainELPositionsTable = ({ elPositions }) => {
-  console.log('elPositions', elPositions);
   const dispatch = useDispatch();
   const gridRef = useRef(null);
 
