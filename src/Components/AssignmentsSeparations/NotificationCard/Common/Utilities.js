145d63da90272b079d032b1f66b223806ccdbbf0
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-10-15 18:08:12 -0400

    Simplified xml building

--- src/Components/AssignmentsSeparations/NotificationCard/Common/Utilities.js
+++ src/Components/AssignmentsSeparations/NotificationCard/Common/Utilities.js
@@ -48,30 +48,6 @@ export const generatePDF = (getPreviewText, filename, memo) => {
 };
 
 /**
- * Helper function to append Approval Chain XML items to a section
- * @param {*} element document HTML element to append to
- * @param {*} apprOriginator ref array of approval originator objects
- */
-const appendChain = (element, apprOriginator, doc) => (
-  apprOriginator.forEach(a => {
-    if (a.Name || a.EmailAddress || a.OrgName || a.UserId) {
-      var Name = doc.createElement('Name');
-      Name.innerHTML = a.Name;
-      element.appendChild(Name);
-      var EMailAddress = doc.createElement('EMailAddress');
-      EMailAddress.innerHTML = a.EmailAddress;
-      element.appendChild(EMailAddress);
-      var OrgName = doc.createElement('OrgName');
-      OrgName.innerHTML = a.OrgName;
-      element.appendChild(OrgName);
-      var UserID = doc.createElement('UserID');
-      UserID.innerHTML = a.UserId;
-      element.appendChild(UserID);
-    }
-  })
-);
-
-/**
  * Generates XML of TM1 Notification according to FSBID format
  * @param {*} cable cable ref object
  * @param {*} content formatted contents of the note cable
@@ -83,216 +59,167 @@ export const generateXML = (cable, content, subject) => {
   const apprOriginator = cable?.QRY_APPR_ORIGINATOR;
   const routingDistro = cable?.QRY_ROUTING_DISTRO;
 
-  var doc = document.implementation.createDocument('', '', null);
-
-  var SmartPortalMessage = doc.createElement('SmartPortalMessage');
-  SmartPortalMessage.setAttribute('xmlns', 'http://SmartPortalMessage.State.Gov/v1');
-  SmartPortalMessage.setAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
-  SmartPortalMessage.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+  let xmlStr = '<?xml version="1.0">';
+  xmlStr = xmlStr.concat('<SmartPortalMessage xmlns="http://SmartPortalMessage.State.Gov/v1" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">');
 
   // ================== IDENTIFIERS ==================
 
-  var Identifiers = doc.createElement('Identifiers');
-  SmartPortalMessage.appendChild(Identifiers);
-    var MessageID = doc.createElement('MessageID');
-    MessageID.innerHTML = uuid;
-    Identifiers.appendChild(MessageID);
-    var MessageIDOriginal = doc.createElement('MessageIDOriginal');
-    MessageIDOriginal.innerHTML = uuid;
-    Identifiers.appendChild(MessageIDOriginal);
+  xmlStr = xmlStr.concat('<Identifiers>');
+  xmlStr = xmlStr.concat(`<MessageID>${uuid}</MessageID>`);
+  xmlStr = xmlStr.concat(`<MessageIDOriginal>${uuid}</MessageIDOriginal>`);
+  xmlStr = xmlStr.concat('</Identifiers>');
 
   // ================== DTG ==================
 
-  var Dtg = doc.createElement('Dtg');
-  SmartPortalMessage.appendChild(Dtg);
+  xmlStr = xmlStr.concat('<Dtg/>');
 
   // ================== PRECEDENCE ==================
 
-  var Precedence = doc.createElement('Precedence');
-  Precedence.innerHTML = 'Routine';
-  SmartPortalMessage.appendChild(Precedence);
+  xmlStr = xmlStr.concat('<Precedence>Routine</Precedence>');
 
   // ================== CLASSIFICATION ==================
 
-  var Classification = doc.createElement('Classification');
-  SmartPortalMessage.appendChild(Classification);
-    var ClassificationLevel = doc.createElement('ClassificationLevel');
-    ClassificationLevel.innerHTML = 'Unclassified';
-    Classification.appendChild(ClassificationLevel);
-    var ClassificationDate = doc.createElement('ClassificationDate');
-    ClassificationDate.innerHTML = formatDate(date, 'YYYY-MM-DD'); // Generate current datestamp 2024-06-24 format
-    Classification.appendChild(ClassificationDate);
-    var Agency = doc.createElement('Agency');
-    Agency.innerHTML = 'State';
-    Classification.appendChild(Agency);
-    var Office = doc.createElement('Office');
-    Classification.appendChild(Office);
-    var EONumber = doc.createElement('EONumber');
-    EONumber.innerHTML = '13526';
-    Classification.appendChild(EONumber);
-    var Derived = doc.createElement('Derived');
-    Classification.appendChild(Derived);
+  xmlStr = xmlStr.concat('<Classification>');
+  xmlStr = xmlStr.concat('<ClassificationLevel>Unclassified</ClassificationLevel>');
+  xmlStr = xmlStr.concat(`<ClassificationDate>${formatDate(date, 'YYYY-MM-DD')}</ClassificationDate>`);
+  xmlStr = xmlStr.concat('<Agency>State</Agency>');
+  xmlStr = xmlStr.concat('<Office/>');
+  xmlStr = xmlStr.concat('<EONumber>13526</EONumber>');
+  xmlStr = xmlStr.concat('<Derived/>');
+  xmlStr = xmlStr.concat('</Classification>');
 
   // ================== ADDRESSEES ==================
 
-  var Addressees = doc.createElement('Addressees');
-  SmartPortalMessage.appendChild(Addressees);
-    var Addressee = doc.createElement('Addressee');
-    Addressees.appendChild(Addressee);
-    routingDistro.forEach(a => {
-      if (a.DT_CODE === 'A' || a.DT_CODE === 'I') {
-        var Address = doc.createElement('Address');
-        Address.innerHTML = a.DisplayName;
-        Addressee.appendChild(Address);
-        var AddressDisplayName = doc.createElement('AddressDisplayName');
-        AddressDisplayName.innerHTML = a.DisplayName;
-        Addressee.appendChild(AddressDisplayName);
-        var AddressType = doc.createElement('AddressType');
-        AddressType.innerHTML = a.AddressType;
-        Addressee.appendChild(AddressType);
-        var ActionType = doc.createElement('ActionType');
-        ActionType.innerHTML = a.ActionType;
-        Addressee.appendChild(ActionType);
-        var Precedence2 = doc.createElement('Precedence');
-        Precedence2.innerHTML = a.Precendence;
-        Addressee.appendChild(Precedence2);
-        var IsZen = doc.createElement('IsZen');
-        IsZen.innerHTML = 'false';
-        Addressee.appendChild(IsZen);
-      }
-    });
+  xmlStr = xmlStr.concat('<Addressees>');
+  routingDistro.forEach(a => {
+    if (a.DT_CODE === 'A' || a.DT_CODE === 'I') {
+      xmlStr = xmlStr.concat('<Addressee>');
+      xmlStr = xmlStr.concat(`<Address>${a.DisplayName}</Address>`);
+      xmlStr = xmlStr.concat(`<AddressDisplayName>${a.DisplayName}</AddressDisplayName>`);
+      xmlStr = xmlStr.concat(`<AddressType>${a.AddressType}</AddressType>`);
+      xmlStr = xmlStr.concat(`<ActionType>${a.ActionType}</ActionType>`);
+      xmlStr = xmlStr.concat(`<Precedence>${a.Precedence}</Precedence>`);
+      xmlStr = xmlStr.concat('<IsZen>false</IsZen>');
+      xmlStr = xmlStr.concat('</Addressee>');
+    }
+  });
+  xmlStr = xmlStr.concat('</Addressees>');
 
   // ================== APPROVAL CHAIN ==================
 
-  var ApprovalChain = doc.createElement('ApprovalChain');
-  SmartPortalMessage.appendChild(ApprovalChain);
-    var Drafter = doc.createElement('Drafter');
-    ApprovalChain.appendChild(Drafter);
-    appendChain(Drafter, apprOriginator, doc);
-    var Approver = doc.createElement('Approver');
-    ApprovalChain.appendChild(Approver);
-    appendChain(Approver, apprOriginator, doc);
-    var Releaser = doc.createElement('Releaser');
-    ApprovalChain.appendChild(Releaser);
-    appendChain(Releaser, apprOriginator, doc);
-    var Originator = doc.createElement('Originator');
-    ApprovalChain.appendChild(Originator);
-    apprOriginator.forEach(a => {
-      var OriginatorName = doc.createElement('Originator');
-      OriginatorName.innerHTML = a?.OriginatorName;
-      Originator.appendChild(OriginatorName);
-      var OriginatorRoutingIndicator = doc.createElement('OriginatorRoutingIndicator');
-      Originator.appendChild(OriginatorRoutingIndicator);
-      var OriginatorLocalName = doc.createElement('OriginatorLocalName');
-      OriginatorLocalName.innerHTML = a?.ORIGINATORLOCALNAME;
-      Originator.appendChild(OriginatorLocalName);
-    });
+  xmlStr = xmlStr.concat('<ApprovalChain>');
+  xmlStr = xmlStr.concat('<Drafter>');
+  apprOriginator.forEach(a => {
+    xmlStr = xmlStr.concat(`<Name>${a.Name}</Name>`);
+    xmlStr = xmlStr.concat(`<EMailAddress>${a.EmailAddress}</EMailAddress>`);
+    xmlStr = xmlStr.concat(`<OrgName>${a.OrgName}</OrgName>`);
+    xmlStr = xmlStr.concat(`<UserID>${a.UserId}</UserID>`);
+  });
+  xmlStr = xmlStr.concat('</Drafter>');
+  xmlStr = xmlStr.concat('<Approver>');
+  apprOriginator.forEach(a => {
+    xmlStr = xmlStr.concat(`<Name>${a.Name}</Name>`);
+    xmlStr = xmlStr.concat(`<EMailAddress>${a.EmailAddress}</EMailAddress>`);
+    xmlStr = xmlStr.concat(`<OrgName>${a.OrgName}</OrgName>`);
+    xmlStr = xmlStr.concat(`<UserID>${a.UserId}</UserID>`);
+  });
+  xmlStr = xmlStr.concat('</Approver>');
+  xmlStr = xmlStr.concat('<Releaser>');
+  apprOriginator.forEach(a => {
+    xmlStr = xmlStr.concat(`<Name>${a.Name}</Name>`);
+    xmlStr = xmlStr.concat(`<EMailAddress>${a.EmailAddress}</EMailAddress>`);
+    xmlStr = xmlStr.concat(`<OrgName>${a.OrgName}</OrgName>`);
+    xmlStr = xmlStr.concat(`<UserID>${a.UserId}</UserID>`);
+  });
+  xmlStr = xmlStr.concat('</Releaser>');
+  xmlStr = xmlStr.concat('<Originator>');
+  apprOriginator.forEach(a => {
+    xmlStr = xmlStr.concat(`<OriginatorName>${a.OriginatorName}</OriginatorName>`);
+    xmlStr = xmlStr.concat('<OriginatorRoutingIndicator/>');
+    xmlStr = xmlStr.concat(`<OriginatorLocalName>${a.ORIGINATORLOCALNAME}</OriginatorLocalName>`);
+  });
+  xmlStr = xmlStr.concat('</Originator>');
+  xmlStr = xmlStr.concat('</ApprovalChain>');
 
   // ================== CONTENT ==================
 
-  var Content = doc.createElement('Content');
-  SmartPortalMessage.appendChild(Content);
-    var BodyPlainText = doc.createElement('BodyPlainText');
-    BodyPlainText.innerHTML = `<![CDATA[${content}]]>`;
-    Content.appendChild(BodyPlainText);
-    var Subject = doc.createElement('Subject');
-    Subject.innerHTML = subject;
-    Content.appendChild(Subject);
+  xmlStr = xmlStr.concat('<Content>');
+  xmlStr = xmlStr.concat(`<BodyPlainText><![CDATA[${content}]]></BodyPlainText>`);
+  xmlStr = xmlStr.concat(`<Subject>${subject}</Subject>`);
+  xmlStr = xmlStr.concat('</Content>');
 
   // ================== PROPERTIES ==================
 
-  var Properties = doc.createElement('Properties');
-  SmartPortalMessage.appendChild(Properties);
-    var ReleaseDateTime = doc.createElement('ReleaseDateTime');
-    ReleaseDateTime.innerHTML = date.toISOString(); // Convert timestamp 2024-06-24T00:00:00Z format
-    Properties.appendChild(ReleaseDateTime);
-    var MessageType = doc.createElement('MessageType');
-    MessageType.innerHTML = 'OrgAuthority';
-    Properties.appendChild(MessageType);
-    var TransmitComputerName = doc.createElement('TransmitComputerName');
-    Properties.appendChild(TransmitComputerName);
-    var TransmitSystem = doc.createElement('TransmitSystem');
-    Properties.appendChild(TransmitSystem);
-    var CorrectionReason = doc.createElement('CorrectionReason');
-    Properties.appendChild(CorrectionReason);
-    var AdHocProperties = doc.createElement('AdHocProperties');
-    Properties.appendChild(AdHocProperties);
-      var Key = doc.createElement('Key');
-      Key.innerHTML = 'SystemSource';
-      AdHocProperties.appendChild(Key);
-      var Value = doc.createElement('Value');
-      Value.innerHTML = 'FSBID';
-      AdHocProperties.appendChild(Value);
-    var AdHocProperties2 = doc.createElement('AdHocProperties');
-    Properties.appendChild(AdHocProperties2);
-      var Key2 = doc.createElement('Key');
-      Key2.innerHTML = 'SchemaSource';
-      AdHocProperties2.appendChild(Key2);
-      var Value2 = doc.createElement('Value');
-      Value2.innerHTML = 'SmartPortalMessage';
-      AdHocProperties2.appendChild(Value2);
+  xmlStr = xmlStr.concat('<Properties>');
+  xmlStr = xmlStr.concat(`<ReleaseDateTime>${date.toISOString()}</ReleaseDateTime>`);
+  xmlStr = xmlStr.concat('<MessageType>OrgAuthority</MessageType>');
+  xmlStr = xmlStr.concat('<TransmitComputerName/>');
+  xmlStr = xmlStr.concat('<TransmitSystem/>');
+  xmlStr = xmlStr.concat('<CorrectionReason/>');
+  xmlStr = xmlStr.concat('<AdHocProperties>');
+  xmlStr = xmlStr.concat('<Key>SystemSource</Key>');
+  xmlStr = xmlStr.concat('<Value>FSBID</Value>');
+  xmlStr = xmlStr.concat('</AdHocProperties>');
+  xmlStr = xmlStr.concat('<AdHocProperties>');
+  xmlStr = xmlStr.concat('<Key>SchemaSource</Key>');
+  xmlStr = xmlStr.concat('<Value>SmartPortalMessage</Value>');
+  xmlStr = xmlStr.concat('</AdHocProperties>');
+  xmlStr = xmlStr.concat('</Properties>');
 
   // ========== CLASSIFICATION MODIFICATION DRIVER ==========
 
-  var ClassificationModificationDriver = doc.createElement('ClassificationModificationDriver');
-  ClassificationModificationDriver.setAttribute('xsi:nil', 'true');
-  SmartPortalMessage.appendChild(ClassificationModificationDriver);
+  xmlStr = xmlStr.concat('<ClassificationModificationDriver xsi:nil="true"/>');
 
   // ================== SENSITIVITY CODE ==================
 
-  var SensitivityCode = doc.createElement('SensitivityCode');
-  SensitivityCode.innerHTML = 'Privacy/PII';
-  SmartPortalMessage.appendChild(SensitivityCode);
+  xmlStr = xmlStr.concat('<SensitivityCode>Privacy/PII</SensitivityCode>');
 
   // ================== REL TO MARKINGS ==================
 
-  var RelToMarkings = doc.createElement('RelToMarkings');
-  SmartPortalMessage.appendChild(RelToMarkings);
+  xmlStr = xmlStr.concat('<RelToMarkings/>');
 
   // ================== REFERENCES ==================
 
-  var References = doc.createElement('References');
-  SmartPortalMessage.appendChild(References);
+  xmlStr = xmlStr.concat('<References/>');
 
   // ================== CAPTIONS ==================
 
-  var Captions = doc.createElement('Captions');
-  SmartPortalMessage.appendChild(Captions);
-    var Caption = doc.createElement('Caption');
-    Captions.appendChild(Caption);
-      var ValueC = doc.createElement('Value');
-      ValueC.innerHTML = 'TM CHANNEL';
-      Caption.appendChild(ValueC);
+  xmlStr = xmlStr.concat('<Captions>');
+  xmlStr = xmlStr.concat('<Caption>');
+  xmlStr = xmlStr.concat('<Value>TM CHANNEL</Value>');
+  xmlStr = xmlStr.concat('</Caption>');
+  xmlStr = xmlStr.concat('</Captions>');
 
   // ================== TAGS ==================
 
-  var Tags = doc.createElement('Tags');
-  SmartPortalMessage.appendChild(Tags);
-    var Tag = doc.createElement('Tag');
-    Tags.appendChild(Tag);
-      var ValueT = doc.createElement('Value');
-      ValueT.innerHTML = 'APER';
-      Tag.appendChild(ValueT);
-      var TagType = doc.createElement('TagType');
-      TagType.innerHTML = 'S';
-      Tag.appendChild(TagType);
-    var Tag2 = doc.createElement('Tag');
-    Tags.appendChild(Tag2);
-      var ValueT2 = doc.createElement('Value');
-      ValueT2.innerHTML = 'AFIN';
-      Tag2.appendChild(ValueT2);
-      var TagTypeT2 = doc.createElement('TagType');
-      TagTypeT2.innerHTML = 'S';
-      Tag2.appendChild(TagTypeT2);
-    var PassLines = doc.createElement('PassLines');
-    Tags.appendChild(PassLines);
-    routingDistro.forEach(a => {
-      var PassLine = doc.createElement('PassLine');
-      PassLine.innerHTML = a;
-      PassLines.appendChild(PassLine);
-    });
+  xmlStr = xmlStr.concat('<Tags>');
+  xmlStr = xmlStr.concat('<Tag>');
+  xmlStr = xmlStr.concat('<Value>APER</Value>');
+  xmlStr = xmlStr.concat('<TagType>S</TagType>');
+  xmlStr = xmlStr.concat('</Tag>');
+  xmlStr = xmlStr.concat('<Tag>');
+  xmlStr = xmlStr.concat('<Value>AFIN</Value>');
+  xmlStr = xmlStr.concat('<TagType>S</TagType>');
+  xmlStr = xmlStr.concat('</Tag>');
+  xmlStr = xmlStr.concat('</Tags>');
 
-  doc.appendChild(SmartPortalMessage);
+  // ================== PASSLINES ==================
 
-  return doc;
+  xmlStr = xmlStr.concat('<PassLines>');
+  routingDistro.forEach(a => {
+    xmlStr = xmlStr.concat(`<PassLine>${a.PASSLINE}<PassLine>`);
+  });
+  xmlStr = xmlStr.concat('</PassLines>');
+
+
+  xmlStr = xmlStr.concat('</SmartPortalMessage>');
+
+  // const parser = new window.DOMParser();
+  // const xmlp = parser.parseFromString(xmlStr, 'text/xml');
+  // console.log(xmlp);
+  // const serializer = new window.XMLSerializer();
+  // const xml = serializer.serializeToString(doc);
+  // console.log(xml);
+
+  return xmlStr;
 };
