5b50fe450e00425993fdf921445307f86ce38c7b
musiqe31 <agbakwucharles@gmail.com>
2024-10-15 18:30:23 -0500

    testing selections

--- src/actions/bidderPortfolio.js
+++ src/actions/bidderPortfolio.js
@@ -462,8 +462,6 @@ export function bidderPortfolioFetchData(query = {}) {
           batch(() => {
             dispatch(bidderPortfolioLastQuery(query$$, data.count, endpoint));
             dispatch(bidderPortfolioFetchDataSuccess(data));
-            dispatch(bidderPortfolioHasErrored(false));
-            dispatch(bidderPortfolioIsLoading(false));
           });
         })
         .catch((m) => {
