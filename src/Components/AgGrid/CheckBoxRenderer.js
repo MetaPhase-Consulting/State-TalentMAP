4464cef8a40874cae3286f6e07064b44cfffd44d
Felicia Imbriano <fimbriano@metaphaseconsulting.com>
2024-10-18 08:47:29 -0400

    pull out props

--- src/Components/AgGrid/CheckBoxRenderer.js
+++ src/Components/AgGrid/CheckBoxRenderer.js
@@ -2,23 +2,25 @@ import PropTypes from 'prop-types';
 import CheckBox from '../CheckBox';
 
 const CheckboxRenderer = (props) => {
+  const { colDef, node, value } = props;
+
   const onCheckBoxClick = (checked) => {
-    props.node.setDataValue(props.colDef.field, checked);
+    node.setDataValue(colDef.field, checked);
   };
 
   return (
-    <CheckBox {...props} value={props.value} onCheckBoxClick={onCheckBoxClick} />
+    <CheckBox {...props} value={value} onCheckBoxClick={onCheckBoxClick} />
   );
 };
 
 CheckboxRenderer.propTypes = {
-  value: PropTypes.bool.isRequired,
-  node: PropTypes.shape({
-    setDataValue: PropTypes.func.isRequired,
-  }).isRequired,
   colDef: PropTypes.shape({
     field: PropTypes.string.isRequired,
   }).isRequired,
+  node: PropTypes.shape({
+    setDataValue: PropTypes.func.isRequired,
+  }).isRequired,
+  value: PropTypes.bool.isRequired,
 };
 
 export default CheckboxRenderer;
